b3e91e46c083202e6481de678be49ea9806368a9
Alex <alex.baeuerle@uni-ulm.de>
2021-10-14 11:12:41 +0200

    Started using Sphinx for docs

---
+++ docs/source/index.rst
@@ -0,0 +1,14 @@
+Luna
+====
+
+Inspired by `Lucid <https://github.com/tensorflow/lucid>`_, **Luna** is a Feature Visualization package for Tensorflow.
+While Lucid does not support Tensorflow 2, **Luna** was built with Tensorflow 2 at its core.
+
+.. note::
+
+   This project is under active development.
+
+.. toctree::
+   :maxdepth: 2
+
+   api
--- luna/featurevis/relu_grad.py
+++ luna/featurevis/relu_grad.py
@@ -6,10 +6,16 @@ from contextlib import contextmanager
 import uuid
 import tensorflow as tf
 
+
 def redirected_relu_grad(operation, grad):
-    """Compute ReLu gradient
+    """Compute ReLu gradient.
+
+    Args:
+        operation (str): the type of operation to convert, needs to be Relu.
+        grad (tf.Tensor): the gradient tensor.
+
     Returns:
-        Redirected gradients smaller than 0
+        Redirected gradients smaller than 0.
     """
     assert operation.type == "Relu"
     relu_input = operation.inputs[0]
@@ -23,7 +29,8 @@ def redirected_relu_grad(operation, grad):
     redirected_grad = tf.where(neg_pushing_lower, tf.zeros_like(grad), grad)
 
     # Ensure we have at least a rank 2 tensor, as we expect a batch dimension
-    assert_op = tf.Assert(tf.greater(tf.rank(relu_grad), 1), [tf.rank(relu_grad)])
+    assert_op = tf.Assert(tf.greater(
+        tf.rank(relu_grad), 1), [tf.rank(relu_grad)])
     with tf.control_dependencies([assert_op]):
         # only use redirected gradient where nothing got through original gradient
         batch = tf.shape(relu_grad)[0]
@@ -31,16 +38,23 @@ def redirected_relu_grad(operation, grad):
         relu_grad_mag = tf.norm(reshaped_relu_grad, axis=1)
     result_grad = tf.where(relu_grad_mag > 0., relu_grad, redirected_grad)
 
-    global_step_t =tf.compat.v1.train.get_or_create_global_step()
+    global_step_t = tf.compat.v1.train.get_or_create_global_step()
     return_relu_grad = tf.greater(global_step_t, tf.constant(16, tf.int64))
 
     return tf.where(return_relu_grad, relu_grad, result_grad)
 
 #pylint: disable=R0914
+
+
 def redirected_relu6_grad(operation, grad):
-    """[Compute ReLu6 gradients
+    """Compute ReLu6 gradients
+
+    Args:
+        operation (str): the type of operation to convert, needs to be Relu6.
+        grad (tf.Tensor): the gradient tensor.
+
     Returns:
-        Redirected gradients bigger than 6
+        Redirected gradients bigger than 6.
     """
     assert operation.type == "Relu6"
     relu_input = operation.inputs[0]
@@ -58,31 +72,31 @@ def redirected_relu6_grad(operation, grad):
     redirected_grad = tf.where(dir_filter, tf.zeros_like(grad), grad)
 
     # Ensure we have at least a rank 2 tensor, as we expect a batch dimension
-    assert_op = tf.Assert(tf.greater(tf.rank(relu_grad), 1), [tf.rank(relu_grad)])
+    assert_op = tf.Assert(tf.greater(
+        tf.rank(relu_grad), 1), [tf.rank(relu_grad)])
     with tf.control_dependencies([assert_op]):
         # only use redirected gradient where nothing got through original gradient
         batch = tf.shape(relu_grad)[0]
         reshaped_relu_grad = tf.reshape(relu_grad, [batch, -1])
         relu_grad_mag = tf.norm(reshaped_relu_grad, axis=1)
-    result_grad =  tf.where(relu_grad_mag > 0., relu_grad, redirected_grad)
+    result_grad = tf.where(relu_grad_mag > 0., relu_grad, redirected_grad)
 
     global_step_t = tf.compat.v1.train.get_or_create_global_step()
     return_relu_grad = tf.greater(global_step_t, tf.constant(16, tf.int64))
 
     return tf.where(return_relu_grad, relu_grad, result_grad)
 
+
 def register_to_random_name(grad_f):
     """Register a gradient function to a random string.
-    In order to use a custom gradient in TensorFlow, it must be registered to a
-    string. This is both a hassle, and -- because only one function can every be
-    registered to a string -- annoying to iterate on in an interactive
-    environemnt.
-    This function registers a function to a unique random string of the form:
-        {FUNCTION_NAME}_{RANDOM_SALT}
-    And then returns the random string. This is a helper in creating more
-    convenient gradient overrides.
+    In order to use a custom gradient in TensorFlow, it must be registered to a string.
+    This is both a hassle, and -- because only one function can every be registered to a string -- annoying to iterate on in an interactive environemnt.
+    This function registers a function to a unique random string of the form: {FUNCTION_NAME}_{RANDOM_SALT}
+    And then returns the random string. This is a helper in creating more convenient gradient overrides.
+
     Args:
         grad_f: gradient function to register. Should map (op, grad) -> grad(s)
+
     Returns:
         String that gradient function was registered to.
     """
@@ -90,6 +104,7 @@ def register_to_random_name(grad_f):
     grad_f_name = grad_f.__name__ + "_" + str(uuid.uuid4())
     tf.RegisterGradient(grad_f_name)(grad_f)
     return grad_f_name
+
 
 @contextmanager
 def gradient_override_map(override_dict):
@@ -100,6 +115,7 @@ def gradient_override_map(override_dict):
     Example:
         def _foo_grad_alt(op, grad): ...
         with gradient_override({"Foo": _foo_grad_alt}):
+
     Args:
         override_dict: A dictionary describing how to override the gradient.
         keys: strings correponding to the op type that should have their gradient
