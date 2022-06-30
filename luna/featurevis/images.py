638e3a4f33c2ff5bfd0d6625c58a47d89452d036
Daniel Jönsson <daniel.jonsson@liu.se>
2022-06-30 10:39:09 +0200

    refactor image intialization to use number of channels instead of bool

--- luna/featurevis/images.py
+++ luna/featurevis/images.py
@@ -16,7 +16,7 @@ MAX_NORM_SVD_SQRT = np.max(np.linalg.norm(COLOR_CORRELATION_SVD_SQRT, axis=0))
 COLOR_MEAN = [0.48, 0.46, 0.41]
 
 
-def initialize_image(width, height, val_range_top=1.0, val_range_bottom=-1.0):
+def initialize_image(width, height, channels = 3, val_range_top=1.0, val_range_bottom=-1.0):
     """
     Creates an initial randomized image to start feature vis process.
     This could be subject to optimization in the future.
@@ -29,11 +29,13 @@ def initialize_image(width, height, val_range_top=1.0, val_range_bottom=-1.0):
         A randomly generated image.
     """
     print("initializing image")
+    if channels != 1 and channels != 3:
+        raise ValueError("Only 1 and 3 image channels are currently supported.")
     # We start from a gray image with some random noise
     if tf.compat.v1.keras.backend.image_data_format() == "channels_first":
-        img = tf.random.uniform((1, 3, width, height), dtype=tf.dtypes.float32)
+        img = tf.random.uniform((1, channels, width, height), dtype=tf.dtypes.float32)
     else:
-        img = tf.random.uniform((1, width, height, 3), dtype=tf.dtypes.float32)
+        img = tf.random.uniform((1, width, height, channels), dtype=tf.dtypes.float32)
     # rescale values to be in the middle quarter of possible values
     img = (img - 0.5) * 0.25 + 0.5
     val_range = val_range_top - val_range_bottom
@@ -88,7 +90,7 @@ def save_image(img, name=None):
 
 
 def initialize_image_ref(
-    width, height, std=None, fft=True, decorrelate=True, channels=None,  seed=None
+    width, height, channels=3, std=None, fft=True, decorrelate=True,  seed=None
 ):
     """
     Creates an initial randomized image to start feature vis process.
@@ -97,10 +99,10 @@ def initialize_image_ref(
     Args:
         width (int): The width of the image.
         height (int): The height of the image.
+        channels (int): Number of image channels.
         sd (float): standard deviation for noise initialization.
         fft (boolean): Image parameterization with fast fourier transformation.
         deccorelate (boolean): the color interpretation of the image tensor's color.
-        channels (boolean): True for gray images.
         seed (int): Random seed for the image initialization for reproducibility.
 
     Returns:
@@ -109,9 +111,9 @@ def initialize_image_ref(
     print("initializing image")
     # We start from a gray image with some random noise
     if tf.compat.v1.keras.backend.image_data_format() == "channels_first":
-        img = tf.random.uniform((1, 3, width, height), dtype=tf.dtypes.float32)
+        img = tf.random.uniform((1, channels, width, height), dtype=tf.dtypes.float32)
     else:
-        img = tf.random.uniform((1, width, height, 3), dtype=tf.dtypes.float32)
+        img = tf.random.uniform((1, width, height, channels), dtype=tf.dtypes.float32)
 
     if fft:
         image_f = fft_image(img, std=std)
@@ -124,11 +126,13 @@ def initialize_image_ref(
                   img.shape[2], img.shape[3]], scale=std
         ).astype(np.float32)
 
-    if channels:
-        output = tf.nn.sigmoid(image_f)
-    else:
+    if channels == 3:
         output = to_valid_rgb(
             image_f[..., :3], decorrelate=decorrelate, sigmoid=True)
+    else:
+        output = tf.nn.sigmoid(image_f)
+
+        
 
     return output
 
